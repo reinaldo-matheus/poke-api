83aec93f80923ce1b4d483924663bdfd4e0957fd
Matheus <reinaldomatheus011@gmail.com>
2024-11-16 00:27:36 -0300

    correção e melhorias no arquivo css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
+import { MagnifyingGlass } from "photosphor-icons/react";
 import axios from 'axios';
+import "./App.css"
+
 
 import './App.css';
 
 function App() {
   const [pokemons, setPokemons] = useState({});
 
-  const getPokemons = (id) => {  
+  const getPokemons = (id) => {
     axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
       .then(response => {
         const pokemon = response.data;
@@ -23,15 +26,30 @@ function App() {
   }, []);
 
   console.log("deu certo", pokemons);
-  
+
   return (
     <div>
       <h1>Pokedex</h1>
+
+      <div className='busca-container'>
+       <MagnifyingGlass size={40} />
+       <input
+       className='busca'
+       type='search'
+       placeholder='pesquisando pokemons'
+       value={busca}
+       onChange={({target}) => setBusca(target.value)}
+       />
+
+      </div>
       <ul>
-        {Object.values(pokemons).map(item => (
+        {Object.values(pokemons).map(({ id, name, types }) => (
           <li>
-           {item.id}. {item.name}
-           <img src = {item.sprites.front_default}/>
+            {item.id}. {item.name}
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`} alt={name} />
+            <h2>{id}. {name}</h2>
+
+            <p>{item.types.map(item => item.type.name).join(" || ")}</p>
           </li>
         ))}
       </ul>
@@ -40,3 +58,4 @@ function App() {
 }
 
 export default App;
+
